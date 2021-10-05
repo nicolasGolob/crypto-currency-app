96685ab154064e4f0a27728b268ef20ffbd22a22
Nicolas Golob <nicolasgolob@gmail.com>
2021-10-05 18:20:17 +0200

    retrieve some data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [item, setItem] = useState([]);
+  const data = process.env.REACT_APP_API_KEY
+
+  useEffect(()=>{
+    (async function(){
+      const response = await fetch([data]);
+      const dataReponse = await response.json();
+      if(response.ok){
+        setItem(dataReponse);
+      }else{
+        alert('An error has occurred');
+      }
+    })()
+  },[data])
   return (
     <div className="App">
-      <h1>the beginning of the project..</h1>
+      <>
+      <table>
+          <thead>
+              <tr>
+                  <th>Symbol</th>
+                  <th>Name</th>
+              </tr>
+          </thead>
+          <tbody>
+             {item.map(i=><tr key={i.id}>
+                  <td>{i.symbol}</td>
+                  <td>{i.name}</td>                 
+              </tr>)} 
+          </tbody>
+      </table>
+      </>
     </div>
   );
 }
