d51557494f88b8c8868c56a23ecf577b4853fb6c
Nicolas Golob <nicolasgolob@gmail.com>
2021-10-06 11:13:10 +0200

    adding some data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TableData from './TableData';
 
 function App() {
   const [item, setItem] = useState([]);
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState([]);
   const data = process.env.REACT_APP_API_KEY
 
   useEffect(()=>{
@@ -18,32 +18,31 @@ function App() {
       }
     })()
   },[data])
-
   function handleSearch(event){
     setSearch(event.target.value);
   }
 
-  const filterBitcoins = item.filter(item=>
-    item.name.toLowerCase().includes(search)
+  const cryptoSearch = item.filter(items=>
+    items.name.toLowerCase().includes(search)
   )
-
   return (
     <>
     <div className="crypto-app">
       <div className="crypto-search"> 
-        <p>Search a bitcoin with her data</p>
+        <p>Search a bitcoin with her name</p>
         <form>
           <input type="text" className="search-input" onChange={handleSearch} placeholder="Search a crypto"/>
         </form>
       </div>
-      {filterBitcoins.map(i=> 
+      {cryptoSearch.map(i=> 
         <TableData 
           key={i.id} 
           name={i.name}
           image={i.image}
           symbol={i.symbol}
           current_price={i.current_price}
-          market_cap_change_percentage_24h={i.market_cap_change_percentage_24h}
+          price_change_24h={i.price_change_24h}
+          price_change_percentage_24h={i.price_change_percentage_24h}
           />
         )}
     </div>
--- src/TableData.js
+++ src/TableData.js
@@ -1,17 +1,24 @@
 import React from 'react'
 
-export default function TableData({name, image, symbol, current_price, market_cap_change_percentage_24h}) {
+
+export default function TableData({name, image, symbol, current_price, price_change_24h, price_change_percentage_24h}) {
     return (
         <>
             <div className="table-container">
                 <div className="table-body">
                     <div className="table-coin">
-                        <h1>{name}</h1>   
-                        <img src={image} alt='symbol'width={50} height={50}/>
+                        <img src={image} alt='symbol'width={55} height={55}/>
+                        <h4>{name}</h4>   
                         <p className="coin-symbol">{symbol}</p>   
                             <div className="coin-data">
-                                <p>{current_price}</p>
-                                <p>{market_cap_change_percentage_24h}%</p>
+                                <p className="crypto-current-price">{current_price}€</p>
+                                <p className=""> + {price_change_24h}€</p>
+                                {
+                                    price_change_percentage_24h < 0 ?
+                                    (<p className="crypto-percent-red">{price_change_percentage_24h}%</p>) 
+                                        :  
+                                    (<p className="crypto-percent-green">+{price_change_percentage_24h}%</p>)                            
+                                }
                             </div>                 
                     </div>
                 </div>
