71f26ad510ea05fa9586bddc9180f9846b1e74e5
Nicolas Golob <nicolasgolob@gmail.com>
2021-10-07 09:50:26 +0200

    add style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import TableData from './TableData';
+import './TableData.css';
 
 function App() {
   const [item, setItem] = useState([]);
@@ -21,31 +23,46 @@ function App() {
   function handleSearch(event){
     setSearch(event.target.value);
   }
-
   const cryptoSearch = item.filter(items=>
     items.name.toLowerCase().includes(search)
   )
   return (
     <>
-    <div className="crypto-app">
-      <div className="crypto-search"> 
-        <p>Search a bitcoin with her name</p>
-        <form>
-          <input type="text" className="search-input" onChange={handleSearch} placeholder="Search a crypto"/>
+    <div className="container py-3">
+      <div className="row"> 
+        <form className="col-12 text-center">
+          <h3>Search a bitcoin with her name</h3>
+          <input type="text" className="search-input mb-2" onChange={handleSearch} placeholder="Search a crypto"/>
         </form>
       </div>
-      {cryptoSearch.map(i=> 
-        <TableData 
-          key={i.id} 
-          name={i.name}
-          image={i.image}
-          symbol={i.symbol}
-          current_price={i.current_price}
-          price_change_24h={i.price_change_24h}
-          price_change_percentage_24h={i.price_change_percentage_24h}
-          />
-        )}
     </div>
+    <div class="container">
+      <div class="pt-3 justify-content-center text-center row">
+          <div class="table-responsive-lg col-lg-12 col-md-12 col-sm-12">
+              <table class="table text-center">
+                <tr>
+                  <th className="pb-3">Logo</th>
+                  <th className="pb-3">Name</th>
+                  <th className="pb-3">Symbol</th>
+                  <th className="pb-3">Current Price</th>
+                  <th className="pb-3">Price Change 24h</th>
+                  <th className="pb-3">Price Change Percentage</th>
+                </tr>                             
+                  {cryptoSearch.map(i=> 
+                    <TableData 
+                      key={i.id} 
+                      name={i.name}
+                      image={i.image}
+                      symbol={i.symbol}
+                      current_price={i.current_price}
+                      price_change_24h={i.price_change_24h}
+                      price_change_percentage_24h={i.price_change_percentage_24h}
+                      />
+                    )}
+                </table>
+              </div>
+          </div>
+      </div>
     </>
   );
 }
--- src/TableData.js
+++ src/TableData.js
@@ -1,28 +1,43 @@
-import React from 'react'
-
+import React from 'react';
+import './TableData.css';
 
 export default function TableData({name, image, symbol, current_price, price_change_24h, price_change_percentage_24h}) {
     return (
         <>
-            <div className="table-container">
-                <div className="table-body">
+            <tbody className="my-3 text-center">   
+                <tr>
+                    <td><img src={image} alt='symbol' className="pt-2"/></td>
+                    <td className="pt-4">{name}</td>
+                    <td className="pt-4">{symbol}</td>
+                    <td className="pt-4">{current_price.toFixed(2)}€</td>
+                    <td className="pt-4">{price_change_24h.toFixed(2)}€</td>
+                    {
+                        price_change_percentage_24h < 0 ?
+                        (<td className="crypto-percent-red pt-4">{price_change_percentage_24h.toFixed(2)}%</td>) 
+                            :  
+                        (<td className="crypto-percent-green pt-4">+{price_change_percentage_24h.toFixed(2)}%</td>)                            
+                    }
+                </tr>
+            </tbody>         
+        </>
+    )
+}
+
+
+ /* <div className="table-row">
                     <div className="table-coin">
-                        <img src={image} alt='symbol'width={55} height={55}/>
-                        <h4>{name}</h4>   
+                        <img src={image} alt='symbol'/>
+                        <h4 className="coin-title">{name}</h4>   
                         <p className="coin-symbol">{symbol}</p>   
-                            <div className="coin-data">
                                 <p className="crypto-current-price">{current_price}€</p>
-                                <p className=""> + {price_change_24h}€</p>
+                                <p className="">{price_change_24h}€</p>
                                 {
                                     price_change_percentage_24h < 0 ?
                                     (<p className="crypto-percent-red">{price_change_percentage_24h}%</p>) 
                                         :  
                                     (<p className="crypto-percent-green">+{price_change_percentage_24h}%</p>)                            
                                 }
-                            </div>                 
                     </div>
                 </div>
-            </div>         
-        </>
-    )
-}
+            </div>          */
+
